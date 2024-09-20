50da9c2260ea476116a21f15a1599bacd118d418
LEANDRO JUNGES LEMES <leandro_j_lemes@estudante.sesisenai.org.br>
2024-09-19 22:07:06 -0300

    first commit : tada:

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+       <header>Cabeçalho</header>
+       <aside>Lado Esquerdo</aside>
+       <main>Principal</main>
+       <footer>Rodapé</footer>
+    </>
+  )
+}
+
+export default App
